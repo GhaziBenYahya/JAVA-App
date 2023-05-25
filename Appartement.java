a6fa26249e57138a478a07a2e7c828ad33e34cf7
GhaziBenYahya <ghazibenyahya2@gmail.com>
2023-05-25 17:02:38 +0200

    first commit

---
+++ Appartement.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapp;
+
+/**
+ *
+ * @author GMI
+ */
+public class Appartement {
+    
+    private int num_app;
+private String type;
+private String disp;
+private String prix;
+private int cin_c;
+public int getnum_app() {
+return num_app;
+}
+public void setnum_app(int num_app) {
+this.num_app = num_app;
+}
+public String gettype() {
+return type;
+}
+public void settype(String type) {
+this.type = type;
+}
+
+public String getdisp() {
+return type;
+}
+public void setdisp(String n) {
+this.disp = n;}
+
+public String getprix() {
+return prix;
+}
+public void setprix(String p) {
+this.prix = p;
+}
+
+public String getcin_c() {
+return type;
+}
+public void setcin_c(int c) {
+this.cin_c = c;
+}
+
+    
+}
---
+++ Clients.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapp;
+
+/**
+ *
+ * @author GMI
+ */
+public class Clients {
+    
+    private int cin;
+private String nom;
+private String prenom;
+private String date_cont ;
+public int getcin() {
+return cin;
+}
+public void setcin(int cin) {
+this.cin = cin;
+}
+public String getNom() {
+return nom;
+}
+public void setNom(String nom) {
+this.nom = nom;
+}
+public String getPrenom() {
+return prenom;
+}
+public void setPrenom(String prenom)
+{ this.prenom = prenom;
+}
+public String getdate_cont() {
+return date_cont;
+}
+public void setdate_cont(String dat)
+{ this.date_cont = dat;
+}
+} 
+    
